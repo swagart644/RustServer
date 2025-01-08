a7ec8735e45db3d8c632c21ee4b8d9d7f139e0da
Brad Swagart <bswagart@betterworldbooks.com>
2025-01-08 10:05:04 -0500

    Added some small features with appstate with mutex included

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,43 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::sync::Mutex;
+
+struct AppState {
+    app_name: String,
+    counter: Mutex<i32>,
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let state = web::Data::new(AppState {
+        app_name: String::from("rust_server"),
+        counter: Mutex::new(0)
+    });
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(state.clone())
+            .service(appname)
             .service(hello)
             .service(echo)
+            .service(count)
             .route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
+}
+
+#[get("/counter")]
+async fn count(data: web::Data<AppState>) -> String {
+    let mut counter = data.counter.lock().unwrap();
+    *counter += 1;
+    format!("Request number: {counter}")
+}
+
+#[get("/appname")]
+async fn appname(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name; 
+    format!("Hello {app_name}!")
 }
 
 #[get("/")]
